c3fd09fbaa3cb91765790cb10c18bb8261d73377
kengo2003 <saitokengo2003@gmail.com>
2025-12-19 01:53:33 +0900

    feat: tailwind導入

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 - Rust
 - htmx
 - Tailwind
+
+開発環境ではTailwindを編集した際に再生性が必要
+```
+./tools/tailwindcss -i ./assets/tailwind.css -o ./static/app.css --watch
+```
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.html", "./src/**/*.rs"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
